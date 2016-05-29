6e678d44a8f915e614e5bcff419529b342f8be4a
Kalen Hall <kalenhall@me.com>
2016-05-29 16:33:44 -0400

    + adding message to clients

--- scripts/qotd-onConnect.sh
+++ scripts/qotd-onConnect.sh
@@ -2,6 +2,6 @@
 ################################################################################
 # QOTD On connect script
 ################################################################################
-echo -e "\033[1;32m[Client connected]\033[0m"
+echo -e "\033[1;32m[Client connected - (Control - c) to disconnect]\033[0m"
 cat /home/ttc/Development/int/work/qotd.txt
 nc -l -p 9100 -c /home/ttc/Development/int/scripts/qotd-onConnect.sh
