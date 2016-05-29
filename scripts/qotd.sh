d456050e04bcf3219ab72fc8ac3f714085ccef74
Kalen Hall <kalenhall@me.com>
2016-05-29 16:20:54 -0400

    + Hacks

--- scripts/qotd.sh
+++ scripts/qotd.sh
@@ -3,4 +3,4 @@
 # QOTD Server for port 9100
 ################################################################################
 echo -e "\033[1;32m[Starting the QOTD server on port 9100]\033[0m"
-nc -lk -p 9100 -e cat /home/ttc/Development/int/work/qotd.txt
+nc -l -p 9100 -e /home/ttc/Development/int/scripts/qotd-onConnect.sh
