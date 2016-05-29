f845c08969dd4ae3bb443ce9af52e6c972d58c7f
Kalen Hall <kalenhall@me.com>
2016-05-29 16:38:46 -0400

    + instructions

--- scripts/qotd.sh
+++ scripts/qotd.sh
@@ -2,5 +2,7 @@
 ################################################################################
 # QOTD Server for port 9100
 ################################################################################
+echo -e "\033[1;32m[To view another group's web page type 'w3m ADDRESS' ('q' to quit)]\033[0m"
+echo -e "\033[1;32m[To view another group's QOTD type 'nc ADDRESS 9100' ('Control - c' to quit)]\033[0m"
 echo -e "\033[1;32m[Starting the QOTD server on port 9100 (Control-c) to quit]\033[0m"
 nc -l -p 9100 -c /home/ttc/Development/int/scripts/qotd-onConnect.sh
