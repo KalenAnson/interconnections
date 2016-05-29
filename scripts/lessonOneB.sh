230be6349e3a4c07425a2243006d09c555b22f3c
Kalen Hall <kalenhall@me.com>
2016-05-29 15:04:44 -0400

    + Address

--- scripts/lessonOneB.sh
+++ scripts/lessonOneB.sh
@@ -3,6 +3,6 @@
 # Lesson 1 - B
 # Play
 ################################################################################
-sudo /sbin/ifconfig eth0 172.16.0.102
+sudo /sbin/ifconfig eth0 172.16.0.101
 cd /home/ttc/Development/int/work/
 /bin/bash
