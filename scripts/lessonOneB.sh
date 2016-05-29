e3b5a7387f9653fda869fc68cd7885d1e2f00205
Kalen Hall <kalenhall@me.com>
2016-05-29 15:14:53 -0400

    + updates

--- scripts/lessonOneB.sh
+++ scripts/lessonOneB.sh
@@ -3,6 +3,8 @@
 # Lesson 1 - B
 # Play
 ################################################################################
-sudo /sbin/ifconfig eth0 172.16.0.101
+# Set IP Address
+sudo /sbin/ifconfig eth0 172.16.0.101 netmask 255.255.255.0 up
+#sudo route add default gw 172.16.0.1
 cd /home/ttc/Development/int/work/
 /bin/bash
