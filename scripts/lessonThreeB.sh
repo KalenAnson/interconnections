e7a1ee9a8fdcee2d56f9dadeb468bb72ffe6f305
Kalen Hall <kalenhall@me.com>
2016-05-29 16:43:34 -0400

    + Instructions

--- scripts/lessonThreeB.sh
+++ scripts/lessonThreeB.sh
@@ -4,7 +4,7 @@
 # Instructions
 ################################################################################
 echo -e "\033[1;32m[Use the command 'nano index.html' to change your web content]\033[0m"
-echo -e "\033[1;32m[Use the command 'nano qotd.txt' to change your quote of the day]\033[0m"
+echo -e "\033[1;32m[Use the command 'nano qotd.md' to change your quote of the day]\033[0m"
 echo -e "\033[1;32m[Type '/sbin/ifconfig eth0 ' and the IP address on the board to change your address]\033[0m"
 cd /home/ttc/Development/int/work/
 /bin/bash
--- scripts/qotd-onConnect.sh
+++ scripts/qotd-onConnect.sh
@@ -2,6 +2,9 @@
 ################################################################################
 # QOTD On connect script
 ################################################################################
+# Welcome message
 echo -e "\033[1;32m[Client connected - (Control - c) to disconnect]\033[0m"
-cat /home/ttc/Development/int/work/qotd.txt
+# Send quote
+cat /home/ttc/Development/int/work/qotd.md
+# Relaunch nc since this version does not have '-k' option
 nc -l -p 9100 -c /home/ttc/Development/int/scripts/qotd-onConnect.sh
