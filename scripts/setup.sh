c5b657269b64761c210ddcf1ddfb1bf905d3d660
Kalen Hall <kalenhall@me.com>
2016-05-29 17:12:45 -0400

    + Adding Webserver script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -21,16 +21,19 @@ then
 	adduser ttc sudo
 	echo -e "\033[1;32m[Creating the user's files]\033[0m"
 	git clone https://github.com/kalenanson/interconnections /home/ttc/Development/int
-	echo -e "\033[1;32m[Setting ownership of user's files]\033[0m"
-	chown -R ttc:ttc /home/ttc
 	echo -e "\033[1;32m[Installing necessary packages]\033[0m"
-	apt-get install git vim openssh-server openssh-client apache2 tcpdump nc telnet w3m htop tmux
+	apt-get install git vim openssh-server openssh-client apache2 tcpdump netcat-traditional telnet w3m htop tmux
 	sleep 2
 	echo -e "\033[1;32m[Creating dot files]\033[0m"
 	cp /home/ttc/Development/int/env/.tmux.conf /home/ttc/.tmux.conf
 	printf "PATH=\$PATH:/sbin" >> /home/ttc/.bashrc
+	echo -e "\033[1;32m[Setting up web server]\033[0m"
+	/home/ttc/Development/int/scripts/webserver.sh
+	echo -e "\033[1;32m[Setting ownership of user's files]\033[0m"
+	chown -R ttc:ttc /home/ttc
 	echo -e "\033[1;32m[User creation successful]\033[0m"
-	echo -e "\033[1;32m[Login with the `ttc` username and the password you created]\033[0m"
+	sleep 2
+	echo -e "\033[1;32m[Login with the 'ttc' username and the password you created]\033[0m"
 else
 	echo -e "\033[1;32m[User creation aborted]\033[0m"
 fi
