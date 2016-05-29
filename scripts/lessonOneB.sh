b7f8289b59636c490d9d595bae209c8f4505788b
Kalen Hall <kalenhall@me.com>
2016-05-29 15:33:36 -0400

    + Lesson 2

--- scripts/lessonOneB.sh
+++ scripts/lessonOneB.sh
@@ -4,7 +4,9 @@
 # Play
 ################################################################################
 # Set IP Address
+echo -e "\033[1;32m[Type your password below, you will not see the password as you type]\033[0m"
 sudo /sbin/ifconfig eth0 172.16.0.101 netmask 255.255.255.0 up
 #sudo route add default gw 172.16.0.1
 cd /home/ttc/Development/int/work/
+
 /bin/bash
---
+++ scripts/lessonTwoB.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+################################################################################
+# Lesson 1 - B
+# Play
+################################################################################
+# Set IP Address
+echo -e "\033[1;32m[Type your password above, you will not see the password as you type]\033[0m"
+echo -e "\033[1;32m[Use (CONTROL + a) then Up Arrow or Down Arrow to change screen focus]\033[0m"
+echo -e "\033[1;32m[Servers: `nc -lp 9000`]\033[0m"
+echo -e "\033[1;32m[Clients: `nc ADDRESS 9000` where ADDRESS is another group's address]\033[0m"
+cd /home/ttc/Development/int/work/
+/bin/bash
